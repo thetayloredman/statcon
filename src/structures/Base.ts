665c899e655d44d4c0ecbf8e180dcb30d9ad3d95
Logan Devine <logan@zirco.dev>
2026-04-01 13:20:53 -0700

    feat: add `Base.useMany`

--- src/structures/Base.ts
+++ src/structures/Base.ts
@@ -2,6 +2,10 @@ export class Base {
     use(cb: (thing: this) => this): this {
         return cb(this);
     }
+
+    useMany(...cbs: ((thing: this) => this)[]): this {
+        return cbs.reduce((thing, cb) => cb(thing), this);
+    }
 }
 
 export interface Serialize {
